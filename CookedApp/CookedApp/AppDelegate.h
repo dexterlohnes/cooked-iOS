1cb6e5cbc82298a58839af7cf929316967727120
dexterlohnes <dexterlohnes@gmail.com>
2015-05-07 12:06:37 -0700

    Initial Commit

---
+++ CookedApp/CookedApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CookedApp
+//
+//  Created by Dexter Lohnes on 5/7/15.
+//  Copyright (c) 2015 The Wise City. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CookedApp/CookedApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CookedApp
+//
+//  Created by Dexter Lohnes on 5/7/15.
+//  Copyright (c) 2015 The Wise City. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
